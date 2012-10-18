44d5ac7edaff7e9229f25280ea9f753133e6e4f4
octavr <octavr@gmail.com>
2012-10-19 00:25:22 +0200

    version 2.0 adding callbacks

--- js/jquery.polyglot.language.switcher.js
+++ js/jquery.polyglot.language.switcher.js
@@ -3,16 +3,126 @@
 
 /* ---------------------------------------------------------------------- */
 /* "Polyglot" Language Switcher
-/* ----------------------------------------------------------------------
-Version: 1.4
-Author: Ixtendo
-Author URI: http://www.ixtendo.com
-License: MIT License
-License URI: http://www.opensource.org/licenses/mit-license.php
-------------------------------------------------------------------------- */
+ /* ----------------------------------------------------------------------
+ Version: 2.0
+ Author: Ixtendo
+ Author URI: http://www.ixtendo.com
+ License: MIT License
+ License URI: http://www.opensource.org/licenses/mit-license.php
+ ------------------------------------------------------------------------- */
 
+/**
+ * jquery.timer.js
+ *
+ * Copyright (c) 2011 Jason Chavannes <jason.chavannes@gmail.com>
+ *
+ * http://jchavannes.com/jquery-timer
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 (function ($) {
+
+    //jquery.timer.js
+    $.timer = function(func, time, autostart) {
+        this.set = function(func, time, autostart) {
+            this.init = true;
+            if(typeof func == 'object') {
+                var paramList = ['autostart', 'time'];
+                for(var arg in paramList) {if(func[paramList[arg]] != undefined) {eval(paramList[arg] + " = func[paramList[arg]]");}};
+                func = func.action;
+            }
+            if(typeof func == 'function') {this.action = func;}
+            if(!isNaN(time)) {this.intervalTime = time;}
+            if(autostart && !this.active) {
+                this.active = true;
+                this.setTimer();
+            }
+            return this;
+        };
+        this.once = function(time) {
+            var timer = this;
+            if(isNaN(time)) {time = 0;}
+            window.setTimeout(function() {timer.action();}, time);
+            return this;
+        };
+        this.play = function(reset) {
+            if(!this.active) {
+                if(reset) {this.setTimer();}
+                else {this.setTimer(this.remaining);}
+                this.active = true;
+            }
+            return this;
+        };
+        this.pause = function() {
+            if(this.active) {
+                this.active = false;
+                this.remaining -= new Date() - this.last;
+                this.clearTimer();
+            }
+            return this;
+        };
+        this.stop = function() {
+            this.active = false;
+            this.remaining = this.intervalTime;
+            this.clearTimer();
+            return this;
+        };
+        this.toggle = function(reset) {
+            if(this.active) {this.pause();}
+            else if(reset) {this.play(true);}
+            else {this.play();}
+            return this;
+        };
+        this.reset = function() {
+            this.active = false;
+            this.play(true);
+            return this;
+        };
+        this.clearTimer = function() {
+            window.clearTimeout(this.timeoutObject);
+        };
+        this.setTimer = function(time) {
+            var timer = this;
+            if(typeof this.action != 'function') {return;}
+            if(isNaN(time)) {time = this.intervalTime;}
+            this.remaining = time;
+            this.last = new Date();
+            this.clearTimer();
+            this.timeoutObject = window.setTimeout(function() {timer.go();}, time);
+        };
+        this.go = function() {
+            if(this.active) {
+                this.action();
+                this.setTimer();
+            }
+        };
+
+        if(this.init) {
+            return new $.timer(func, time, autostart);
+        } else {
+            this.set(func, time, autostart);
+            return this;
+        }
+    };
 
     $.fn.polyglotLanguageSwitcher = function (op) {
 
@@ -24,7 +134,7 @@ License URI: http://www.opensource.org/licenses/mit-license.php
         var ulElement = $("<ul class=\"dropdown\">");
         var length = 0;
         var isOpen = false;
-        var liElements = new Array();
+        var liElements = [];
         var settings = $.extend({}, ls.defaults, op);
         var closePopupTimer;
         var isStaticWebSite = settings.websiteType == 'static';
@@ -36,21 +146,31 @@ License URI: http://www.opensource.org/licenses/mit-license.php
         init();
         installListeners();
 
+        function triggerEvent(evt) {
+            if(settings[evt.name]){
+                settings[evt.name].call($(this), evt);
+            }
+        }
+
         function open() {
+            triggerEvent({name:'beforeOpen', element:rootElement, instance:ls});
             aElement.addClass("active");
             doAnimation(true);
             setTimeout(function () {
                 isOpen = true;
+                triggerEvent({name:'afterOpen', element:rootElement, instance:ls});
             }, 100);
         }
 
         function close() {
+            triggerEvent({name:'beforeClose', element:rootElement, instance:ls});
             doAnimation(false);
             aElement.removeClass("active");
             isOpen = false;
             if (closePopupTimer && closePopupTimer.active) {
                 closePopupTimer.clearTimer();
             }
+            triggerEvent({name:'afterClose', element:rootElement, instance:ls});
         }
 
         function suspendCloseAction() {
@@ -198,10 +318,8 @@ License URI: http://www.opensource.org/licenses/mit-license.php
                 liElement = $("<li><a id=\"" + id + "\" href=\"" + href + "\">" + text + "</a></li>");
             }
             liElement.bind('click', function () {
+                triggerEvent({name:'onChange', selectedItem: $(this).children(":first").attr('id'), element:rootElement, instance:ls});
                 doAction($(this));
-                if (settings.callback) {
-                    settings.callback.call($(this), $(this).children(":first").attr('id'));
-                }
             });
             if (settings.openMode == 'hover') {
                 liElement.hover(function () {
@@ -246,6 +364,13 @@ License URI: http://www.opensource.org/licenses/mit-license.php
             }
         }
 
+        ls.open = function () {
+            open();
+        };
+        ls.close = function () {
+            close();
+        };
+        triggerEvent({name:'afterLoad', element:rootElement, instance:ls});
     };
 
     var ls = $.fn.polyglotLanguageSwitcher;
@@ -259,7 +384,12 @@ License URI: http://www.opensource.org/licenses/mit-license.php
         indexPage:'index.html',
         websiteType:'dynamic',
         testMode:false,
-        callback:NaN
+        onChange:NaN,
+        afterLoad:NaN,
+        beforeOpen:NaN,
+        afterOpen:NaN,
+        beforeClose:NaN,
+        afterClose:NaN
     };
 
 
